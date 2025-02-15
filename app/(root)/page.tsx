72b3d70191c394318080fe04c3b77758db14eb6a
John Mathew <jmathew@acilogistix.com>
2025-02-15 19:01:25 +0530

    hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,16 @@ export default function Home() {
   console.log("sex");
   return (
     <>
+
+
+    <section className="pink_container">
     <h1 className="heading">IDEAS WILL COME TO LIFE AND THAT IS THE INTENTION</h1>
+    <p className="sub-heading !max-w-3xl">Spot outlier academic papers that rise unexpectedly in influence.</p><br></br>
+    <p className="sub-heading !max-w-3xl">Be part of the discovery and showcase your ideas for <strong>Decentralized Peer Review</strong></p>
+
+
+
+    </section>
     </>
 
     
